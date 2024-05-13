d70b3319d8f83bf559b8719d78b7c3aa58e3f447
YasiraNBD <155614875+ynbdissanayake@users.noreply.github.com>
2024-05-13 19:19:56 +0530

    Commit Final Project

--- app/src/main/java/com/example/structured/NotedatabaseHelper.kt
+++ app/src/main/java/com/example/structured/NotedatabaseHelper.kt
@@ -8,7 +8,7 @@ class NotedatabaseHelper(context:Context) :SQLiteOpenHelper(context,DATABASE_NAM
 
 
     companion object{
-        private const val DATABASE_NAME = "db"
+        private const val DATABASE_NAME = "ToDoPlus.db"
         private const val DATABASE_VERSION = 1
         private const val TABLE_NAME = "allnotes"
         private const val COLUMN_ID = "id"
